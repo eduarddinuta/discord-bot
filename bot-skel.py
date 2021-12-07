c8235c0078b4eeeb6f47c66bb57135864620c718
Eduard Dinuta <eduard.dinuta@stud.acs.upb.ro>
2021-12-07 17:38:40 +0200

    feature
    Signed-off-by: Eduard Dinuta <eduard.dinuta@stud.acs.upb.ro>

--- bot-skel.py
+++ bot-skel.py
@@ -1,4 +1,4 @@
-
+#edit for token flag
 #Copyright 2021 Eduard Dinuta
    # This program is free software: you can redistribute it and/or modify
    # it under the terms of the GNU General Public License as published by
